ff445afd62c274c20b9e849de587adcfe3e30cde
codeskiii <161380586+codeskiii@users.noreply.github.com>
2024-02-27 18:30:32 +0100

    Update sandbox.py

--- sandbox.py
+++ sandbox.py
@@ -1,46 +1,42 @@
 import numpy as np
-import os
+import matplotlib.pyplot as plt
 
 class sandbox():
-    def train(bets: int, model_0: list, ans: list):  # Added self as the first argument
-        for bet in range(bets):
-            a = np.random.choice(ans)
+    def progress_bar(progress, total,name):
+        percent = int(100 * (progress / total))
+        bar = "=" * percent + '-' * (100 - percent)
+        print(f"\r{bar}| {name} {percent:.2f}%", end="\r")
 
-            if a == 1:
-                model_0.append(1)
-            elif a == 0:
-                model_0.append(0)
+    def f(x):
+        return x**2 - 1/8 * x**4
 
-            print("TRAINING", (bet / bets) * 100, "%")
+    def train(x,y):  # Added self as the first argument
+        for deg in [1, 2, 3]:
+            reg = np.polyfit(x, y, deg=deg)  # Fit polynomial model
+            y_predicted = np.polyval(reg, x)  # Evaluate model predictions
+            MSE = np.mean(((y - y_predicted) ** 2))  # Calculate Mean Squared Error
 
-        print("TRAINING ENDED")
-        return model_0
+            print(f"deg={deg} | MSE={MSE:.5f}")  # Print degree and MSE
 
-    def test(trained_model: list, attempts: int, ans: list):
-        score = 0
-        for attempt in range(attempts):
-            a = np.random.choice(ans)
-            s = np.random.choice(trained_model)
+            plt.figure(figsize=(10,6))
+            plt.plot(x,y, "ro", label="Some data")
+            plt.plot(x, np.polyval(reg, x), label=f"deg={deg}")
+            plt.show()
 
-            if a == s:
-                score += 1
-
-            print("TESTING", (attempt / attempts) * 100, "%")
-
-        print("TESTING ENDED")
-        return score
+    def test():
+        pass
 
     def run():
-        model_0 = [0, 1]
-        # 1 - 2/3   0 - 1/3
-        ans = [1, 1, 0, 0, 0]
-        bets = 100_000
-        trained_model = sandbox.train(bets, model_0, ans)
+        # sequence
+        X = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+        Y = [sandbox.f(x) for x in X]
+        #bets = 1_000_000
+        trained_model = sandbox.train(X, Y)
 
-        attempts = 25_000
-        results = sandbox.test(trained_model, attempts, ans)
+        #attempts = 10_000
+        #results = sandbox.test()
 
-        print("ACCURACY", (results / attempts) * 100, "%" )
+        #print(f"\r ACCURACY {round((results/attempts * 100),5)}% ", end="\r")
 
 sandbox.run()
 #rl = np.array([np.array([epoch() for _ in range(15)]) for _ in range(50)])
