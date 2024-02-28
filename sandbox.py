34500ee3990f1382a4b65aa3b5622c53830cf9e5
codeskiii <161380586+codeskiii@users.noreply.github.com>
2024-02-28 14:01:56 +0100

    Update sandbox.py

--- sandbox.py
+++ sandbox.py
@@ -1,17 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 class sandbox():
-    def progress_bar(progress, total,name):
+    def progress_bar(progress, total, name):
         percent = int(100 * (progress / total))
         bar = "=" * percent + '-' * (100 - percent)
         print(f"\r{bar}| {name} {percent:.2f}%", end="\r")
 
-    def f(x):
-        return x**2 - 1/8 * x**4
+    def f(x:int):
+        z = (x ** 3 + (x * 1j))
+        return z
 
     def train(x,y):  # Added self as the first argument
-        for deg in [1, 2, 3]:
+        for deg in range(1, 13):
             reg = np.polyfit(x, y, deg=deg)  # Fit polynomial model
             y_predicted = np.polyval(reg, x)  # Evaluate model predictions
             MSE = np.mean(((y - y_predicted) ** 2))  # Calculate Mean Squared Error
@@ -28,7 +30,7 @@ class sandbox():
 
     def run():
         # sequence
-        X = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+        X = [x for x in range(1, 1_000_000)]
         Y = [sandbox.f(x) for x in X]
         #bets = 1_000_000
         trained_model = sandbox.train(X, Y)
